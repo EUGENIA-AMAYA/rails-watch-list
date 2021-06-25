a261ef307f1f693c51f6c2d5eddf5aa34cf957d1
Eugenia Amaya <eugenia@estudioamaya.net>
2021-06-25 16:58:51 -0300

    movies

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,7 +1,7 @@
 class BookmarksController < ApplicationController
-  before_action :set_list, only: [:new, :created]
-  before_action :set_bookmark, only: [:destroy]
-  
+  before_action :set_bookmark, only: :destroy
+  before_action :set_list, only: [:new, :create]
+
   def new
     @bookmark = Bookmark.new
   end
@@ -10,7 +10,7 @@ class BookmarksController < ApplicationController
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
     if @bookmark.save
-      redirect_to lists_path
+      redirect_to list_path(@list)
     else
       render :new
     end
@@ -18,16 +18,20 @@ class BookmarksController < ApplicationController
 
   def destroy
     @bookmark.destroy
-    redirect_to list_path(@list)
+    redirect_to list_path(@bookmark.list)
   end
 
   private
 
-  def set_list
-    @list = List.find(params[:list_id])
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 
-  def bookmark_params
-    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
   end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,14 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :destroy]
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @bookmark = Bookmark.new
+  end
+
   def new
     @list = List.new
   end
@@ -6,23 +16,24 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to lists_path
+      redirect_to list_path(@list)
     else
       render :new
     end
   end
 
-  def show
-    @list = List.find(params[:id])
-  end
-
-  def index
-    @lists = List.all
+  def destroy
+    @list.destroy
+    redirect_to lists_path
   end
 
   private
 
+  def set_list
+    @list = List.find(params[:id])
+  end
+
   def list_params
-    params.require(:list).permit(:list_name)
+    params.require(:list).permit(:name)
   end
 end
